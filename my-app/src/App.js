5adf1da7f1d60180e761cfb589f5630277590ec3
Shayne Geilman <shaynegeilman@gmail.com>
2023-02-25 19:21:50 +0800

    prepared component structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,4 +4,15 @@ function App() {
   return <div></div>;
 }
 
+//main form.
+function Form() {}
+
+//component for the cards. Will show back or front based on what side prop is passed.
+function Card({ side }) {
+  return <div></div>;
+}
+
+//successful submission component
+function Submit() {}
+
 export default App;
