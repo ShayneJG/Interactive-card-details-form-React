22876ce2ea094f21178a93e5614eef8f257cd91f
Shayne Geilman <shaynegeilman@gmail.com>
2023-02-27 17:47:24 +0800

    input and error handling setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -38,6 +38,16 @@ function App() {
 
 //main form.
 function Form({ details, setDetails, setComplete }) {
+  const [error, setError] = useState({
+    numError: false,
+    mmError: false,
+    yyError: false,
+    cvcError: false,
+  });
+  const inputUpdate = (event) => {
+    const { name, value } = event.target;
+    setDetails({ ...details, [name]: value });
+  };
   const submission = () => {
     //
     //  TODO: add error handling here.
@@ -53,6 +63,7 @@ function Form({ details, setDetails, setComplete }) {
           name="name"
           placeholder="e.g. Jane Appleseed"
           type="text"
+          onChange={inputUpdate}
         />
         <label for="number">CARD NUMBER</label>
         <input
@@ -60,15 +71,36 @@ function Form({ details, setDetails, setComplete }) {
           name="number"
           placeholder="e.g. 1234 5678 9123 0000"
           type="text"
+          onChange={inputUpdate}
         />
+        <p className="error">Wrong format, numbers only</p>
         <label for="month">EXP. DATE (MM/YY)</label>
-        <input id="month" name="month" placeholder="MM" type="text" />
+        <input
+          id="month"
+          name="month"
+          placeholder="MM"
+          type="text"
+          onChange={inputUpdate}
+        />
         <label for="year" className="sr-only">
           year
         </label>
-        <input id="year" name="year" placeholder="YY" type="text" />
+        <input
+          id="year"
+          name="year"
+          placeholder="YY"
+          type="text"
+          onChange={inputUpdate}
+        />
+        <p className="error">Can't be blank</p>
         <label for="cvc">CVC</label>
-        <input id="cvc" name="cvc" placeholder="e.g. 123" />
+        <input
+          id="cvc"
+          name="cvc"
+          placeholder="e.g. 123"
+          onChange={inputUpdate}
+        />
+        <p className="error">Wrong format, numbers only</p>
         <button onClick={submission}>Confirm</button>
       </form>
     </div>
