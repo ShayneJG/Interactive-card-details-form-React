2fd4625fb8ca97466e122453477b7f280d7d618c
Shayne Geilman <shaynegeilman@gmail.com>
2023-03-01 11:38:04 +0800

    extracted numError

--- my-app/src/components/errorHandler.js
+++ my-app/src/components/errorHandler.js
@@ -2,12 +2,17 @@ const errorHandler = (setError, details, error, defaultError) => {
   setError(defaultError);
   let errorCheck = false;
   //card number error check
-  const regex = /^[0-9]*$/;
-  if (!regex.test(details.number)) {
-    setError({ ...error, numError: true });
+  if (numError(setError, error, details)) {
     errorCheck = true;
   }
   return errorCheck;
 };
 
+const numError = (setError, error, details) => {
+  const regex = /^[0-9]*$/;
+  if (!regex.test(details.number)) {
+    setError({ ...error, numError: true });
+    return true;
+  }
+};
 export default errorHandler;
